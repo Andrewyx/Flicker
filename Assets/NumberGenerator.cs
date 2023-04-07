96d0d8bf19cb38fb22c837a9687708d6ea85525c
TMZero-c <alexmeimarcus@gmail.com>
2023-04-06 23:45:05 -0700

    Interactable works
    read the title

--- Assets/NumberGenerator.cs
+++ Assets/NumberGenerator.cs
@@ -1,12 +1,33 @@
 using UnityEngine;
-
+using UnityEngine.UI;  
+using System.Collections;
+using System.Collections.Generic;
 
 public class NumberGenerator : MonoBehaviour, IInteractable {
 
-   
-    //public TextTest;
+    public float cooldownTimer = 1.5f;
+    private float currentTime;
+    public Canvas Prompt;
 
     public void Interact() {
-        //TextTest.enabled = true;
+        Debug.Log("Player is near the note");
+        Prompt.enabled = true;
+        currentTime = cooldownTimer;
     }
+
+    private void Start(){
+        currentTime = cooldownTimer;
+
+    }
+
+    private void FixedUpdate(){
+        if(Prompt.enabled){
+            currentTime-=Time.deltaTime;
+            if(currentTime<=0f){
+                Prompt.enabled = false;
+            }
+        }
+
+    }
+
 }
